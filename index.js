92fb44eeabccc59bfded187834299f75b5a77c09
MichaelDelcambre <delcambrem@gmail.com>
2021-11-29 11:54:54 -0600

    done

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Your code goes here
+// document.addEventListener('DOMContentLoaded', () => {
+//     console.log('This is really cool!')
+// })
+// console.log('Before DOM Loads')
+
+
+
+// console.log(document.getElementsByTagName('text').textContent)
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const text = document.getElementById('text').textContent = "This is really cool!"
+})
+
+
+// const text = document.getElementById('text')
+// text.textContent = "This is really cool!"
+// console.log(text)
